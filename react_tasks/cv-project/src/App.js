62e14a0b7721ad48183b2eb72986756215072cf3
sartzalainen <sanniina.bjorn@gmail.com>
2023-01-11 08:24:42 +0200

    setting up structure

--- react_tasks/cv-project/src/App.js
+++ react_tasks/cv-project/src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
 
 class App extends Component {
   constructor() {
     super();
     /*define state: create a task object, and define task.text to be empty string. define tasks to be empty array to start with */
     this.state = {
-      task: { text: '', id: uniqid() },
+      task: { text: '' },
       tasks: [],
     };
   }
-  render()
-  return;
+}
 
 export default App;
