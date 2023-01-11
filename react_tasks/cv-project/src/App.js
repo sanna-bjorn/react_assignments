7ad58f87794153ff52ead4dd7a17860df83076df
sartzalainen <sanniina.bjorn@gmail.com>
2023-01-11 09:31:28 +0200

    setting up npm

--- react_tasks/cv-project/src/App.js
+++ react_tasks/cv-project/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from '../src/Styles/styles.css';
 
 class App extends Component {
   constructor() {
@@ -9,6 +9,34 @@ class App extends Component {
       task: { text: '' },
       tasks: [],
     };
+  }
+  render() {
+    /*destructure state for cleaner code*/
+    const { task, tasks } = this.state;
+
+    return (
+      <div>
+        {/* adding onSubmitTask funtion to the form */}
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor='generalInput'>General info</label>
+          {/* connect onChange handler to forn input element  */}
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type='text'
+            id='taskInput'
+          />
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type='text'
+            id='taskInput'
+          />
+          <button type='submit'>Submit general info</button>
+        </form>
+        {/* <Overview tasks={tasks} /> */}
+      </div>
+    );
   }
 }
 
