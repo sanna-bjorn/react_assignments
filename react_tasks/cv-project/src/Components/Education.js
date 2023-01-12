cf065b937132687befcddc5f7feb26dacd3cf990
sartzalainen <sanniina.bjorn@gmail.com>
2023-01-12 19:37:09 +0200

    small fixes, setting up Education section

---
+++ react_tasks/cv-project/src/Components/Education.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { NoPrint, Print } from 'react-easy-print';
+
+/*take info from props and map over them, display in list element */
+const Education = () => {
+  const [educationInfo, setEducationInfo] = useState({
+    schoolName: '',
+    degree: '',
+    graduated: '',
+  });
+
+  const [editMode, setEditMode] = useState(true);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEducationInfo((prevInfo) => {
+      return { ...prevInfo, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEditMode((prevMode) => !prevMode);
+  };
+
+  const { schoolName, degree, graduated } = educationInfo;
+
+  //   if (!editMode) {
+  //     return (
+  //       <TextSection
+  //         firstName={firstName}
+  //         lastName={lastName}
+  //         email={email}
+  //         phone={phone}
+  //         handleEdit={handleSubmit}
+  //       />
+  //     );
+  //   }
+
+  return (
+    <NoPrint>
+      <Print printOnly>
+        <p className='warning'>Fill in the form.</p>
+      </Print>
+      <section className='educationSection'>
+        <form className='section' action='' onSubmit={handleSubmit}>
+          <label>
+            <p>School name:</p>
+            <input
+              type='text'
+              placeholder='School name'
+              name='schoolName'
+              onChange={handleChange}
+              value={schoolName}
+              required
+            />
+          </label>
+
+          <label>
+            <p>Degree:</p>
+            <input
+              type='text'
+              placeholder='Degree'
+              name='degree'
+              onChange={handleChange}
+              value={degree}
+              required
+            />
+          </label>
+
+          <label>
+            <p>Graduated:</p>
+            <input
+              type='number'
+              placeholder='2002'
+              name='graduated'
+              onChange={handleChange}
+              value={graduated}
+              required
+            />
+          </label>
+          <button className='formBtn generalBtn' type='submit'>
+            Save
+          </button>
+        </form>
+      </section>
+    </NoPrint>
+  );
+};
+
+export default Education;
--- react_tasks/cv-project/src/Components/General.js
+++ react_tasks/cv-project/src/Components/General.js
@@ -4,10 +4,10 @@ import { NoPrint, Print } from 'react-easy-print';
 /*take info from props and map over them, display in list element */
 const General = () => {
   const [personalInfo, setPersonalInfo] = useState({
-    firstName: '';
-    lastName: '';
-    email: '';
-    phone: '';
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
   });
 
   const [editMode, setEditMode] = useState(true);
@@ -15,9 +15,9 @@ const General = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setPersonalInfo((prevInfo) => {
-        let newInfo = {...prevInfo, [name]: value}
-        return newInfo;
-    }); 
+      let newInfo = { ...prevInfo, [name]: value };
+      return newInfo;
+    });
   };
 
   const handleSubmit = (e) => {
@@ -27,78 +27,78 @@ const General = () => {
 
   const { firstName, lastName, email, phone } = personalInfo;
 
-  if (!editMode) {
-    return (
-        <TextSection
-            firstName={firstName}
-            lastName={lastName}
-            email={email}
-            phone={phone}
-            handleEdit={handleSubmit}
-        />
-    );
-}
+  //   if (!editMode) {
+  //     return (
+  //       <TextSection
+  //         firstName={firstName}
+  //         lastName={lastName}
+  //         email={email}
+  //         phone={phone}
+  //         handleEdit={handleSubmit}
+  //       />
+  //     );
+  //   }
 
   return (
-		<NoPrint>
-			<Print printOnly>
-				<p className='warning'>Fill in the form.</p>
-			</Print>
-			<section className='generalSection'>
-				<form className='section' action='' onSubmit={handleSubmit}>
-					<label>
-						<p>First Name:</p>
-						<input
-							type='text'
-							placeholder='First Name'
-							name='firstName'
-							onChange={handleChange}
-							value={firstName}
-							required
-						/>
-					</label>
+    <NoPrint>
+      <Print printOnly>
+        <p className='warning'>Fill in the form.</p>
+      </Print>
+      <section className='generalSection'>
+        <form className='section' action='' onSubmit={handleSubmit}>
+          <label>
+            <p>First Name:</p>
+            <input
+              type='text'
+              placeholder='First Name'
+              name='firstName'
+              onChange={handleChange}
+              value={firstName}
+              required
+            />
+          </label>
 
-                    <label>
-						<p>Last Name:</p>
-						<input
-							type='text'
-							placeholder='Last Name'
-							name='lastName'
-							onChange={handleChange}
-							value={lastName}
-							required
-						/>
-					</label>
+          <label>
+            <p>Last Name:</p>
+            <input
+              type='text'
+              placeholder='Last Name'
+              name='lastName'
+              onChange={handleChange}
+              value={lastName}
+              required
+            />
+          </label>
 
-					<label>
-						<p>Email:</p>
-						<input
-							type='email'
-							placeholder='example@gmail.com'
-							name='email'
-							onChange={handleChange}
-							value={email}
-							required
-						/>
-					</label>
-					<label>
-						<p>Phone Number:</p>
-						<input
-							type='tel'
-							name='phone'
-							placeholder='123-456-7890'
-							onChange={handleChange}
-							value={phone}
-							required
-						/>
-					</label>
-					<button className='formBtn generalBtn' type='submit'>
-						Save
-					</button>
-				</form>
-			</section>
-		</NoPrint>
-	);
+          <label>
+            <p>Email:</p>
+            <input
+              type='email'
+              placeholder='example@gmail.com'
+              name='email'
+              onChange={handleChange}
+              value={email}
+              required
+            />
+          </label>
+          <label>
+            <p>Phone Number:</p>
+            <input
+              type='tel'
+              name='phone'
+              placeholder='123-456-7890'
+              onChange={handleChange}
+              value={phone}
+              required
+            />
+          </label>
+          <button className='formBtn generalBtn' type='submit'>
+            Save
+          </button>
+        </form>
+      </section>
+    </NoPrint>
+  );
 };
 
-export default GeneralSection;
+export default General;
